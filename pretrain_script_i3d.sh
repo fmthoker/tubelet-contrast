9dba52d997f1c077af9b344b1c327832461fb0c2
Fida Thoker <fthoker@ivi-h0.ivi.local>
2023-10-05 13:31:05 +0200

    first commit

---
+++ pretrain_script_i3d.sh
@@ -0,0 +1,2 @@
+# Mini-Kinetics
+bash tools/dist_train.sh configs/moco/i3d/pretraining_mini_kinetics.py  4 --data_dir /ssdstore/fmthoker/kinetics_ctp/ --work_dir ./output/moco/i3d_inetics_400/pretraining
---
+++ pretrain_script_r3d.sh
@@ -0,0 +1,2 @@
+# Mini-Kinetics
+bash tools/dist_train.sh configs/moco/r3d_18/pretraining_mini_kinetics.py  4 --data_dir /ssdstore/fmthoker/kinetics_ctp/ --work_dir ./output/moco/r3d_18_mini_kinetics/pretraining
