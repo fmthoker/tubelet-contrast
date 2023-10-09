708af69feece107b7b53d50728612d2795f8213d
Fida Thoker <fthoker@ivi-h0.ivi.local>
2023-10-09 15:32:14 +0200

    updated readme

--- pretrain_script_r3d.sh
+++
@@ -1,2 +0,0 @@
-# Mini-Kinetics
-bash tools/dist_train.sh configs/moco/r3d_18/pretraining_mini_kinetics.py  4 --data_dir /ssdstore/fmthoker/kinetics_ctp/ --work_dir ./output/moco/r3d_18_mini_kinetics/pretraining
---
+++ scripts_pretrain/mini-kinetics/pretrain_script_r21d.sh
@@ -0,0 +1,2 @@
+# Mini-Kinetics
+ bash tools/dist_train.sh configs/moco/r2plus1d_18/pretraining_mini_kinetics.py  4 --data_dir /ssdstore/fmthoker/kinetics_ctp/ --work_dir ./output/moco/r2plus1d_18_mini_kinetics/pretraining_100_epoch_config
